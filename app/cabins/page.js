5d568d2e80625fae208539464313255300028aee
Emil Gamidov <dayde1991@mail.ru>
2024-10-14 15:36:38 +0500

    Initial commit

---
+++ app/cabins/page.js
@@ -0,0 +1,13 @@
+import Counter from "@/app/_components/Counter";
+
+export const metadata = {
+  title: "Cabins",
+};
+
+export default function Page() {
+  return (
+    <div>
+      <h1>Cabins Page</h1>
+    </div>
+  );
+}
