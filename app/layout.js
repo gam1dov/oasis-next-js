8d43758df47331c947215828bb49f07a1e4ebab5
Emil Gamidov <dayde1991@mail.ru>
2024-10-15 11:27:54 +0500

    Added additional routes profile, reservations, layout DeleteReservation, ReservationCard, SelectCountry, SideNavigation, SignInButton, SignOutButton barebones

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,3 @@
-import Logo from "@/app/_components/Logo";
-import Navigation from "@/app/_components/Navigation";
-
 import { Josefin_Sans } from "next/font/google";
 
 const josefin = Josefin_Sans({
@@ -28,8 +25,8 @@ export default function RootLayout({ children }) {
         className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen flex flex-col antialiased relative`}
       >
         <Header />
-        <div className="flex-1 px-8 py-12">
-          <main className="max-w-7xl  mx-auto">{children}</main>
+        <div className="flex-1 px-8 py-12 grid">
+          <main className="max-w-7xl  mx-auto w-full">{children}</main>
         </div>
       </body>
     </html>
